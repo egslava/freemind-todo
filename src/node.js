a5ef1fddfa31c32d1b3d4ebd8248e4aa5de48038
Viacheslav Egorenko <egslava@gmail.com>
2016-12-27 20:17:23 +0300

    added support of stop signs

--- src/node.js
+++ src/node.js
@@ -8,12 +8,38 @@
 
 const instanceOf = require('instance-of');
 
+/*
+{
+    "$": {
+        "CREATED": "1482820675096",
+        "ID": "ID_662031669",
+        "MODIFIED": "1482853982193",
+        "TEXT": "three"
+    },
+    "icon": [{
+        "$": {
+            "BUILTIN": "stop-sign"
+        }
+    }]
+}
+ */
+
+function hasIcon(node, name){
+    if (node.hasOwnProperty('icon')){
+        return node['icon'].find( icon => icon['$']['BUILTIN'] == name)
+    }
+
+    return false;
+}
+
+function isStopped(node){ return hasIcon(node, 'stop-sign')}
 
 function leafs(json){
     let result = [];
 
-    if (json.hasOwnProperty('node')){
+    if ( isStopped(json) ) return [];
 
+    if (json.hasOwnProperty('node')){
         json['node'].forEach( node => result = result.concat(leafs(node)) );
     } else {
         result.push(json['$'])
