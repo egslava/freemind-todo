388eadff19b40f2a9c6d1c27e1e166937f378ba5
Viacheslav Egorenko <egslava@gmail.com>
2016-12-27 07:02:02 +0300

    removed everything about Angular. Decided to do simple NPM-package in pure JS

--- src/app/about/about.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'about',
-    templateUrl: './app/about/about.component.html'
-})
-
-export class AboutComponent {
-}
--- src/app/home/home.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'home',
-    templateUrl: './app/home/home.component.html'
-})
-
-export class HomeComponent {
-}
